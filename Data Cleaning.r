36486d2fbf355e960c6c9ead69581cb4d69c5485
munozbiol <munozbiol@gmail.com>
2026-04-26 14:59:27 +0200

    Environmental data complete

--- Data Cleaning.r
+++ Data Cleaning.r
@@ -33,3 +33,78 @@ str(environmental_raw)
 
 environmental_raw <- environmental_raw %>%
     mutate_if(is.character, as.factor)
+
+View(environmental_raw)
+
+#The names of MAR2013-2016 were changed in excel due to the program change it 
+# to march-2013. now they are MAG2012-2016. So we need to restore the original names
+
+environmental_raw |> 
+mutate(Code = recode(Code,
+    "MAG2012" = "MAR2012",
+"MAG2013" = "MAR2013",
+"MAG2014" = "MAR2014",
+"MAG2015" ="MAR2015",
+"MAG2016" ="MAR2016"
+)) -> environmental_raw
+
+environmental_raw  <- 
+environmental_raw|> 
+mutate(Code = as.factor(Code))
+
+#Creating the reservoir_id to pass from code to 
+#reservoir name only "MEQ2015" to "MEQ"
+
+environmental_raw <- 
+environmental_raw  |>  
+  extract(
+    col = Code,
+    into = c("reservoir_id", "Year"),
+    regex = "^([A-Za-z]+)([0-9]{4})$",
+    remove = FALSE,
+    convert = TRUE
+  )  |> 
+  mutate(reservoir_id = as.factor(reservoir_id)) |> 
+  mutate(Year = as.factor(Year))
+
+
+#Ok now we have the environmental dataset in a wide format
+#In case any new variable is added in future samplings 
+#to avoid to modify the db structure is needed to pass it to a long format
+
+environmental_long  <- 
+environmental_raw   |>  
+tidyr::pivot_longer(
+    cols = -c("Sampling_id", #the PK
+    where(is.factor)), # selecting all categorical columns
+    names_to = "Variable",
+    values_to = "Value"
+)
+
+
+#Fixing some names in the db 
+
+environmental_long |> 
+mutate(Variable = recode(Variable,
+    "Temp" = "Temperature",
+    "Phytoplankton" = "Phyto_abundance",
+    "Fito.Biomasa" = "Phyto_biomass",
+    "Phycocianin" = "Phycocyanin",
+    "Conduct" = "Conductivity",
+    "Amonium" = "Ammonium",
+    "SS" = "Suspended_solids",
+    "Photic.Zone" = "Photic_zone",
+    "reservoir.percentage" = "Reservoir_percentage",
+    "volume.max" = "Volume_max"
+)) -> environmental_long
+
+View(environmental_long)
+
+#Ok now looks okay
+
+#Zooplankton -------
+
+zooplankton_raw <- read.csv("Master_files/zooplankton.csv",
+ header = TRUE)
+
+View(zooplankton_raw)
